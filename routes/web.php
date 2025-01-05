af29f3c2a10fb29f9285fd64d2b6bef43e01db3e
utsab <utsab@duet.ac.bd>
2025-01-05 20:06:43 +0600

    frontend setup

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     //return "Hi Utsab";
     return view('welcome');
+});*/
+
+Route::get('/', function () {
+    //return "Hi Utsab";
+    return view('frontend.index');
 });
 
 Route::controller(AdminController::class)->group(function (){
